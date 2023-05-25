8e516d95cd19b2e24d6bce47fdd6c8219705eb3e
TonyK_lein <94446571+Tony-Klein@users.noreply.github.com>
2023-05-25 17:03:36 +0200

    Create setup.sh

---
+++ Linux/setup.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# Update package manager apt and upgrade system 
+sudo apt update
+sudo apt full-upgrade --yes
+# autoremove unnötig
+
+# Update package manager apt-get and upgrade system 
+sudo apt-get update
+sudo apt-get dist-upgrade --yes
+
+# Installing Software
+
+## https://wiki.ubuntuusers.de/Python/
+sudo apt-get install python3 --yes
+## sudo apt-get install python-is-python3 --yes
+
+## https://packages.debian.org/search?keywords=python3-dev
+sudo apt install python3-dev --yes
+
+## https://wiki.ubuntuusers.de/pip/
+sudo apt-get install python3-pip --yes
+## python3 -m pip [OPTION(EN)] BEFEHL MODUL(E) 
+## python3 -m pip -r requirements.txt
+
+
+## https://askubuntu.com/questions/1372562/how-to-install-libpq-dev-14-0-1-on-ubuntu-21-10
+## sudo apt-get install libpq-dev --yes
+
+## https://git-scm.com/download/linux
+sudo add-apt-repository ppa:git-core/ppa 
+sudo apt update
+sudo apt install git --yes
+
+## https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+#sudo apt-get install git-all --yes
+
+# Prompt for user name in GitHub
+read -p "Enter your name for GitHub: " username
+
+# Prompt for user email in GitHub
+read -p "Enter your email for GitHub: " email
+
+# Set git config with the entered values
+git config --global user.name "$username"
+git config --global user.email "$email"
+
+## https://wiki.ubuntuusers.de/venv/
+sudo apt-get install python3-venv --yes
+
+### Anlegen einer virtuellen Umgebung:
+### python3 -m venv /home/BENUTZER/venv-test
+
+### Aktivieren der virtuellen Ungebung:
+### source /home/BENUTZER/venv-test/bin/activate 
+
+### Deinstallieren der virtuellen Umgebung:
+### Löschen des für die virtuelle Umgebung angelegte Verzeichnisses inklusive Unterverzeichnissen (.venv Folder)
+
+# Update package manager apt and upgrade system 
+sudo apt update
+sudo apt full-upgrade --yes
+
+# Update package manager apt-get and upgrade system 
+sudo apt-get update
+sudo apt-get dist-upgrade --yes
+
+## Install Visual Studion Code
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https --yes
+sudo apt update
+sudo apt install code --yes # or code-insiders
+
+
+## Create Git/NewProject
+
+#!/bin/bash
+
+# Prompt for project name
+read -p "Enter your project name: " newproject
+
+# Create project directory
+cd ~
+sleep 1
+mkdir Git
+sleep 1
+cd Git
+sleep 1
+mkdir "$newproject"
+sleep 1
+cd "$newproject"
+
+# Perform actions within the project directory
+# Add any additional commands or instructions here
+
+# Example: Create a README file
+echo "This is a new project" > README.md
+
+
+# Example: Create a requirements.txt file
+echo "This is a new project" > requirements.txt
+
+# Example: Print the current working directory
+pwd
+
+# Example: List the files in the directory
+ls -la
+
+sleep 1
+
+## Create Virtual Environment
+python3 -m venv .venv
+
+sleep 1
+
+## Activate Virtual Environment
+source .venv/bin/activate
+
+sleep 2
+
+## Install all necessary pip packages within requirements.txt
+pip install -r requirements.txt
